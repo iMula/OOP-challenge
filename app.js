cca4ed368a91202059885487e2a7153e118e3e6e
Seth Mulia <smulia@r2hstudent.com>
2022-02-28 17:02:36 -0500

    coding challenge complete

---
+++ app.js
@@ -0,0 +1,23 @@
+// Create a class called pokemon with a constructer that takes in a name, type, and attack
+// Create a method for this class that console logs that the pokemon used its attack ex: "Pickachu, used Thunderbolt"
+// Instantiate 2 new pokemon then use the attack method for both of them.
+// You can upload a zip file of this code or push it to a github repo
+
+
+class Pokemon {
+    constructor (name, type, attack){
+        this.name = name,
+        this.type = type,
+        this.attack = attack
+    }
+    attackUsed = () =>{
+    return this.name + ", used " + this.attack
+}
+}
+
+
+const pickachu = new Pokemon("Pickachu", "electric", "Thunderbolt");
+const snorlax = new Pokemon("Snorlax", "psychic", "Amnesia")
+
+console.log(pickachu.attackUsed());
+console.log(snorlax.attackUsed());
